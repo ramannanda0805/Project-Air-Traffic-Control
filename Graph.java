92785e7d366e472c61e1846f8ec0852b980cf6e3
ramannanda0805 <ramannanda0805@gmail.com>
2025-04-12 16:30:38 +0530

    Graph.java

--- Graph.java
+++ Graph.java
@@ -1,26 +1,43 @@
 package com.airtrafficcontrol;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Graph {
-    private final int numAirports;
-    private final List<int[]> edges;
 
-    public Graph(int numAirports) {
-        this.numAirports = numAirports;
-        this.edges = new ArrayList<>();
+    public static class Edge {
+        public String source;
+        public String destination;
+        public int weight;
+
+        public Edge(String source, String destination, int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
     }
 
-    public void addEdge(int src, int dest, int weight) {
-        edges.add(new int[]{src, dest, weight});
+    private final Map<String, List<Edge>> adjacencyList = new HashMap<>();
+
+    public void addCity(String city) {
+        adjacencyList.putIfAbsent(city, new ArrayList<>());
     }
 
-    public List<int[]> getEdges() {
-        return edges;
+    public void connectCities(String a, String b, int weight) {
+        addCity(a);
+        addCity(b);
+        adjacencyList.get(a).add(new Edge(a, b, weight));
+        adjacencyList.get(b).add(new Edge(b, a, weight)); // Assuming undirected graph
     }
 
-    public int getNumAirports() {
-        return numAirports;
+    public List<Edge> getEdges(String city) {
+        return adjacencyList.getOrDefault(city, new ArrayList<>());
+    }
+
+    public Map<String, List<Edge>> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public Set<String> getAllCities() {
+        return adjacencyList.keySet();
     }
 }
